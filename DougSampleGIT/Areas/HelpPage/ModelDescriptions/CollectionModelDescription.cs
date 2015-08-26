4778b41ef43823bc810e8711884dd8b92123f551
Doug Riley ABL <dougrileyabl@outlook.com>
2015-08-26 09:47:06 -0400

    changed test

---
+++ DougSampleGIT/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DougSampleGIT.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DougSampleGIT/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DougSampleGIT.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
